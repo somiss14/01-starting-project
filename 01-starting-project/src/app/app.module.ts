8023c2f6d1ff6aa4a934aec7f9ce918e41a9696a
maciek@mint <mcyran1996@gmail.com>
2024-06-05 20:54:52 +0200

    lesson 433 standalone components

---
+++ 01-starting-project/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppComponent} from './app.component';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {DetailsComponent} from "./welcome/details/details.component";
+
+@NgModule({
+  declarations: [AppComponent, WelcomeComponent],
+  imports: [BrowserModule, DetailsComponent],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ 01-starting-project/src/app/welcome/details/details.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import {AnalyticsService} from 'src/app/shared/analytics.service';
+import {SharedModule} from "../../shared/shared.module";
+
+@Component({
+  standalone: true,
+  imports: [SharedModule],
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css'],
+})
+export class DetailsComponent {
+  constructor(private analyticsService: AnalyticsService) {}
+
+  onClick() {
+    this.analyticsService.registerClick();
+  }
+}
